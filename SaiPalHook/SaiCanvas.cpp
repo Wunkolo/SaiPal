cc0e40c9849c68f088487b285865f7b71b9ed5df
DEElekgolo <DEElekgolo@gmail.com>
2014-10-16 01:30:56 -0700

    Vectorized canvas capture using SSE

--- SaiPalHook/SaiCanvas.cpp
+++ SaiPalHook/SaiCanvas.cpp
@@ -5,6 +5,12 @@
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image_write.h"
 
+#include <type_traits>
+
+#include <stdlib.h> // _aligned_malloc
+#include <emmintrin.h> // _mm_set_epi8 etc
+#include <tmmintrin.h> // _mm_shuffle_epi8
+
 SaiCanvas::SaiCanvas()
 {
 }
@@ -37,34 +43,60 @@ bool SaiCanvas::CaptureImage(const std::string Path)
 	}
 	unsigned int LowerPadX = Canvas(0x114).asUint();
 	unsigned int LowerPadY = Canvas(0x118).asUint();
-	//unsigned int HigherPadX = Canvas(0x11C).asUint();
-	//unsigned int HigherPadY = Canvas(0x120).asUint();
-	unsigned int * Buffer = new unsigned int[Width()*Height()];
-
+	unsigned int HigherPadX = Canvas(0x11C).asUint();
+	unsigned int HigherPadY = Canvas(0x120).asUint();
 	Pointer PixelHeap = Canvas[0x30];
 
-	unsigned int StrideX = PixelHeap(1, sizeof(int)).asUint();
+	unsigned int Stride = PixelHeap(1, sizeof(int)).asUint();
 	//unsigned int StrideY = PixelHeap(2, sizeof(int)).asUint();
 	PixelHeap = PixelHeap[0xC];
+	//16 byte aligned buffer
+	unsigned int * Buffer = new unsigned int[Width()*Height()];
 
-	for( unsigned int x = 0; x < Width(); x++ )
+	// BGRA to RGBA
+	__m128i shuffle =
+		_mm_set_epi8(
+		15, 12, 13, 14,
+		11, 8, 9, 10,
+		7, 4, 5, 6,
+		3, 0, 1, 2);
+	//Process four pixels at a time
+
+	for( unsigned int y = 0; y < Height(); y++ )
 	{
-		for( unsigned int y = 0; y < Height(); y++ )
+		for( unsigned int x = 0; x < Width(); x += 4 )
 		{
-			unsigned int Pixel =
-				PixelHeap(
-				(x + LowerPadX) + (y + LowerPadY) * StrideX,
-				sizeof(int)).asUint();
-			//BGRA to RGBA(Little endian)
-			Pixel = (
-				(Pixel & 0x00ff0000) >> 16) |      //Blue
-				((Pixel & 0x000000ff) << 16) |     //Red
-				(Pixel & 0xff00ff00);              //Green+Alpha
-			Buffer[Width() * y + x] = Pixel;
+			__m128i QuadPixel =
+				_mm_loadu_si128((__m128i*)
+				PixelHeap((x + LowerPadX) + (y + LowerPadY) * Stride,
+				sizeof(int)));
+			QuadPixel = _mm_shuffle_epi8(QuadPixel, shuffle);
+			_mm_store_si128(
+				(__m128i*)&Buffer[Width() * y + x],
+				QuadPixel);
 		}
 	}
+
+	//for( unsigned int x = 0; x < Width(); x++ )
+	//{
+	//	for( unsigned int y = 0; y < Height(); y++ )
+	//	{
+	//		unsigned int Pixel =
+	//			PixelHeap(
+	//			(x + LowerPadX) + (y + LowerPadY) * StrideX,
+	//			sizeof(int)).asUint();
+	//		//BGRA to RGBA(Little endian)
+	//		Pixel = (
+	//			(Pixel & 0x00ff0000) >> 16) |      //Blue
+	//			((Pixel & 0x000000ff) << 16) |     //Red
+	//			(Pixel & 0xff00ff00);              //Green+Alpha
+	//		Buffer[Width() * y + x] = Pixel;
+	//	}
+	//}
+
 	if( stbi_write_png(Path.c_str(), Width(), Height(), 4, Buffer, 0) )
 	{
+		delete[] Buffer;
 		return true;
 	}
 	delete[] Buffer;
