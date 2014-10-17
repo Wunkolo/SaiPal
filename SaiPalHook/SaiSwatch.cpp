e2954c3137bacf42aa0fa5d1ac4b5fa5a674ed68
DEElekgolo <DEElekgolo@gmail.com>
2014-10-17 00:41:14 -0700

    Inlined pixel conversion

--- SaiPalHook/SaiSwatch.cpp
+++ SaiPalHook/SaiSwatch.cpp
@@ -5,14 +5,6 @@
 #define SwatchWidth 12
 #define SwatchHeight 13
 
-inline unsigned int endian_swap(unsigned const int& x)
-{
-	return (((x & 0x000000FF) << 24) |
-			((x & 0x0000FF00) << 8) |
-			((x & 0x00FF0000) >> 8) |
-			((x & 0xFF000000) >> 24));
-}
-
 SaiSwatch::SaiSwatch()
 {
 }
