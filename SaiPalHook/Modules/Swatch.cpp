59ad8436edb3d3ac41ba1e4a903ace1a868d286a
DEElekgolo <DEElekgolo@gmail.com>
2014-10-17 21:53:15 -0700

    Swatch saving and loading, revision 1

--- SaiPalHook/Modules/Swatch.cpp
+++ SaiPalHook/Modules/Swatch.cpp
@@ -17,8 +17,9 @@ std::string Swatch::Info() const
 		"Swatch Directory: " + (SaiPal::Instance().GetDirectory() + "Swatches\n") +
 		"'swatch' shows info of operations you can execute onto your active swatch\n"
 		"'swatch save' Saves your current swatch\n"
-		"'swatch save [Swatchname] (Scale)' Saves your current swatch with the specified name(as png). Scale value optional\n"
-		"'swatch load [Filename]' Loads the specified image into your swatches";
+		"'swatch save [Swatch Name] (Scale)' Saves your current swatch with the specified name.\n"
+		"\tScale value optional(defaults to 1)\n"
+		"'swatch load [Swatch Namename]' Loads the specified image into your swatches";
 }
 
 void Swatch::Tick(const std::chrono::duration<double>& Delta)
@@ -39,14 +40,20 @@ void Swatch::Run(const std::vector<std::string>& Args)
 			if( Args.size() >= 3 )
 			{
 				Path += Args[2];
+				Path += ".png";
 				int Scale = 1;
 				if( Args.size() >= 4 )
 				{
 					Scale = std::atoi(Args[3].c_str());
 					if( Scale <= 0 )
 					{
-						std::cout << "Invalid Scale: " << Scale << std::endl;
+						std::cout << "Invalid Scale: " << Args[3] << std::endl;
 						return;
+					}
+					if( Scale > 100 )
+					{
+						// should be a good enough cut-off.
+						std::cout << "Scale too large!" << std::endl;
 					}
 				}
 				if( SaiPal::Instance().GetSession().
@@ -63,31 +70,100 @@ void Swatch::Run(const std::vector<std::string>& Args)
 			}
 			else
 			{
-				LARGE_INTEGER time;
-				QueryPerformanceCounter(&time);
-				Path += "Swatch [" + std::to_string(time.QuadPart) + "].png";
-				if( SaiPal::Instance().GetSession().
-				   GetSwatch()
-				   .SaveSwatch(Path) )
-				{
-					std::cout << "Swatch saved to: " << Path << std::endl;
-					return;
-				}
-				else
-				{
-					std::cout << "Failed to save swatch image" << std::endl;
-					return;
-				}
+				std::cout << "No swatch name specified" << std::endl;
+				//LARGE_INTEGER time;
+				//QueryPerformanceCounter(&time);
+				//Path += "Swatch [" + std::to_string(time.QuadPart) + "].png";
+				//if( SaiPal::Instance().GetSession().
+				//   GetSwatch()
+				//   .SaveSwatch(Path) )
+				//{
+				//	std::cout << "Swatch saved to: " << Path << std::endl;
+				//	return;
+				//}
+				//else
+				//{
+				//	std::cout << "Failed to save swatch image" << std::endl;
+				//	return;
+				//}
 			}
 		}
 		else if( !Args[1].compare("load") )
 		{
 			if( Args.size() >= 3 )
 			{
+				unsigned int Attrib = GetFileAttributes(
+					(SaiPal::Instance().GetDirectory()
+					+ "Swatches\\"
+					+ Args[2] + ".png").c_str());
+				//Check if file exists.
+				if( Attrib == INVALID_FILE_ATTRIBUTES ||
+				   Attrib == FILE_ATTRIBUTE_DIRECTORY )
+				{
+					std::cout
+						<< "Swatch " << Args[2] << " not found"
+						<< std::endl;
+					std::cout << "Available Swatches: " << std::endl;
+					WIN32_FIND_DATA Finder;
+					HANDLE hFind = FindFirstFile(
+						(SaiPal::Instance().GetDirectory() + "Swatches\\*.png").c_str(),
+						&Finder);
+					if( hFind == INVALID_HANDLE_VALUE )
+					{
+						std::cout << "No swatch files found" << std::endl;
+						return;
+					}
+					do
+					{
+						if( !(Finder.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) )
+						{
+							std::string SwatchName(Finder.cFileName);
+							//remove extension
+							SwatchName = SwatchName.substr(
+								0,
+								SwatchName.find_last_of('.'));
+							std::cout << "-" << SwatchName << std::endl;
+						}
+					} while( FindNextFile(hFind, &Finder) != 0 );
+					return;
+				}
+				if( SaiPal::Instance().GetSession()
+				   .GetSwatch().LoadSwatch(SaiPal::Instance().GetDirectory()
+				   + "Swatches\\"
+				   + Args[2] + ".png") )
+				{
+					std::cout << "Swatch \"" << Args[2] << "\" loaded" << std::endl;
+				}
+				else
+				{
+					std::cout << "Failed to load swatch: " << Args[2] << std::endl;
+				}
 			}
 			else
 			{
-				std::cout << "No swatch file specified" << std::endl;
+				std::cout << "No swatch name specified" << std::endl;
+				std::cout << "Available Swatches: " << std::endl;
+				WIN32_FIND_DATA Finder;
+				HANDLE hFind = FindFirstFile(
+					(SaiPal::Instance().GetDirectory() + "Swatches\\*.png").c_str(),
+					&Finder);
+				if( hFind == INVALID_HANDLE_VALUE )
+				{
+					std::cout << "No swatch files found" << std::endl;
+					return;
+				}
+				do
+				{
+					if( !(Finder.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) )
+					{
+						std::string SwatchName(Finder.cFileName);
+						//remove extension
+						SwatchName = SwatchName.substr(
+							0,
+							SwatchName.find_last_of('.'));
+						std::cout << "-" << SwatchName << std::endl;
+					}
+				} while( FindNextFile(hFind, &Finder) != 0 );
 				return;
 			}
 		}
