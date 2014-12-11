4365ab31e8059104ae5e2956461fff300bfa9f49
DEElekgolo <DEElekgolo@gmail.com>
2014-12-11 11:31:49 -0800

    Layer cycler test

--- SaiPalHook/Modules/Test.cpp
+++ SaiPalHook/Modules/Test.cpp
@@ -5,6 +5,7 @@
 
 Test::Test()
 {
+	Running = false;
 }
 Test::~Test()
 {
@@ -17,17 +18,31 @@ std::string Test::Info() const
 
 void ProcessLayer(Pointer Layer)
 {
-	printf("honk %x\n", Layer(0x5b).asUshort());
+#if defined(SAI120)
+	*(short*)Layer(0x5b - 8) = !Layer(0x5b - 8).asUchar();
+
+	if( Layer(0x5b - 8).asUshort() == 1 )
+	{
+		*(short*)Layer(0xB4 - 8) = 0x90;
+	}
+	else
+	{
+		*(short*)Layer(0xB4 - 8) = 0;
+	}
+#elif defined(SAI110)
 	*(short*)Layer(0x5b) = !Layer(0x5b).asUchar();
 
 	if( Layer(0x5b).asUshort() == 1 )
 	{
-		*(short*)Layer(0x5b) = 0x90;
+		*(short*)Layer(0xB4) = 0x90;
 	}
 	else
 	{
-		*(short*)Layer(0x5b) = 0;
+		*(short*)Layer(0xB4) = 0;
 	}
+#else
+#endif
+
 	if( Layer[0] )
 	{
 		ProcessLayer(Layer[0]);
@@ -46,8 +61,31 @@ void Test::Tick(const std::chrono::duration<double>& Delta)
 		{
 			SaiCanvas Canvas(
 				SaiPal::Instance().GetSession().ActiveCanvas()[0]);
+			printf("honk %x %x\n",
+				   Canvas.GetWindow(),
+				   SaiPal::Instance().GetSession().GetLayerListWindow());
 			Pointer Layer = Canvas.GetFirstLayer();
 			ProcessLayer(Layer);
+
+			RedrawWindow(SaiPal::Instance().GetSession().GetLayerListWindow(),
+						 0,
+						 0,
+						 RDW_INVALIDATE);
+
+			//redraw preview
+			RedrawWindow(SaiPal::Instance().GetSession().GetPreviewWindow(),
+						 0,
+						 0,
+						 RDW_INVALIDATE);
+			PostMessageA(SaiPal::Instance().GetSession().GetPreviewWindow(),
+						 WM_USER + 3066, 0, 0);
+
+			//redraw canvas
+			RedrawWindow(Canvas.GetWindow(),
+						 0,
+						 0,
+						 RDW_INVALIDATE);
+			PostMessageA(Canvas.GetWindow(), WM_USER + 3066, 0, 0);
 		}
 	}
 }
